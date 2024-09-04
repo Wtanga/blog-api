75d9a34c0c5f76855f092b80f442f30efb255bb8
Wtanga <pkkk.2011@mail.ru>
2024-09-05 03:14:41 +0700

    add more tests, add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,27 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+use App\Models\Post;
+use Exception;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @throws Exception
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        for ($i = 0; $i < 5; $i++) {
+            $post = Post::factory()
+                ->create();
+            $count = 5;
+            for ($j = 0; $j < $count; $j++) {
+                Comment::factory()->create(['post_id' => $post->id]);
+            }
+        }
     }
 }
